33d9e2a41a9a49688a08dca76fdd9f2a012d661a
Chien Tran <chientrm@gmail.com>
2021-12-16 22:38:43 +0700

    fix eref run

--- ERef.js
+++ ERef.js
@@ -1,14 +1,11 @@
 import { EObject } from "./EObject.js";
+import { e_search } from "./e_search.js";
 
 export class ERef extends EObject {
   constructor(name) {
     super({ name });
   }
   run(contexts) {
-    for (let i = contexts.length - 1; i >= 0; i--) {
-      const value = contexts[i][this.name];
-      if (value !== undefined) return value;
-    }
-    return undefined;
+    return e_search(contexts, this.name);
   }
 }
