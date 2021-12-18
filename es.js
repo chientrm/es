9c3afb1a6e4467c0aa00040cdc7f78f0312779bb
Chien Tran <chientrm@gmail.com>
2021-12-18 22:44:44 +0700

    build script with rollup

--- es.js
+++ es.js
@@ -1,8 +1,23 @@
 import { ESource } from "./ESource.js";
 import { e_parse_set_contents } from "./e_parser.js";
 
+export const lib = {};
+
 export const e_eval = (contexts, filename, text) => {
   const source = new ESource(filename, text);
   const set = e_parse_set_contents(source);
   return set.run(contexts);
 };
+
+typeof window !== "undefined" &&
+  window.addEventListener("load", () =>
+    document
+      .querySelectorAll("script[type='application/es']")
+      .forEach((script) =>
+        script.src
+          ? fetch(script.src)
+              .then((res) => res.text())
+              .then((text) => e_eval([lib], script.src, text))
+          : e_eval([lib], window.location.href, script.innerHTML)
+      )
+  );
