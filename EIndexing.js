6bc9c2389892f31dac91c0073dc6e107550db07e
Chien Tran <chientrm@gmail.com>
2021-12-18 09:00:41 +0700

    optimize code

--- EIndexing.js
+++ EIndexing.js
@@ -7,11 +7,12 @@ export class EIndexing extends EObject {
   }
   run(contexts) {
     let a = e_search(contexts, this.name);
+    a || this.invalid(this.name);
     !a && this.invalid(this.name);
     const values = this.operands.map((o) => (o.run ? o.run(contexts) : o));
     values.forEach((v) => {
       a = a[v];
-      !a && this.outOfRange(v);
+      a || this.outOfRange(v);
     });
     return a;
   }
