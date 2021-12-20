2f6b04f8aca6b5259b0f68ec6ad7b6d7ad502521
Chien Tran <chientrm@gmail.com>
2021-12-20 09:24:28 +0700

    remove redundant code

--- EIndexing.js
+++ EIndexing.js
@@ -8,7 +8,6 @@ export class EIndexing extends EObject {
   run(contexts) {
     let a = e_search(contexts, this.name);
     a || this.invalid(this.name);
-    !a && this.invalid(this.name);
     const values = this.operands.map((o) => (o.run ? o.run(contexts) : o));
     values.forEach((v) => {
       a = a[v];
